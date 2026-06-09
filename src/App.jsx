c3811f646fc2b961f0316095e65ac1d30cd1a578
sam mclamb <sam.mclambiv@gmail.com>
2026-06-08 22:46:05 -0400

    what the fuck was that error

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,32 @@ const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 
-const API_OPTIONS = {
+const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer: ${API_KEY}`
+    Authorization: `Bearer ${API_KEY}`
   }
-}
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState(null); 
-  useEffect(() => {
-    fetchMovies();
-  }, []);
   const fetchMovies = async () => {
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-      const response = await fetch(endpoint, API_OPTIONS);
-      data = await response.json();
+      const response = await fetch(endpoint, options);
+      const data = await response.json();
       console.log(data);
+      console.log(API_KEY);
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please tryy again sometime");
     }
     }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   
   return (
     <main>
